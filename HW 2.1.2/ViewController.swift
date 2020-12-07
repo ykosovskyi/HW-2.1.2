dc64e4c290d815ccc8a9cab27f3ae6ef55bf55b4
Yevhen Kosovskyi <yevhen.kosovskyi@gmail.com>
2020-12-07 02:32:16 +0200

    Initial Commit

---
+++ HW 2.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.1.2
+//
+//  Created by Евгений Косовский on 07.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
