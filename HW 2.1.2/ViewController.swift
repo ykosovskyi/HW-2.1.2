eedfdb2ebd36803eafbd15cdf44dd7c986c388b0
Yevhen Kosovskyi <yevhen.kosovskyi@gmail.com>
2020-12-07 03:04:48 +0200

    Home Work Done

--- HW 2.1.2/ViewController.swift
+++ HW 2.1.2/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var orangeView: UIView!
+    @IBOutlet var greenView: UIView!
+    
+    @IBOutlet var button: UIButton!
+    
+    var currentColor = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redView.layer.cornerRadius = 75
+        orangeView.layer.cornerRadius = 75
+        greenView.layer.cornerRadius = 75
+        button.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func buttonClick() {
+        if(currentColor == 0) {
+            button.setTitle("Next", for: .normal)
+        }
+        
+        switch(currentColor) {
+        case 1: redView.alpha = 0.3
+        case 2: orangeView.alpha = 0.3
+        case 3: greenView.alpha = 0.3
+        default: break
+        }
+        
+        if(currentColor < 3) {
+            currentColor += 1
+        }
+        else {
+            currentColor = 1
+        }
+        
+        switch(currentColor) {
+        case 1: redView.alpha = 1
+        case 2: orangeView.alpha = 1
+        case 3: greenView.alpha = 1
+        default: break
+        }
+        
+        
+    }
+    
 }
 
